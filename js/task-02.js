fcdf748897b9ef2c2d4ba0d3e472723403289d27
Dmytro Kondratskiy <levius2@gmail.com>
2021-06-13 19:05:21 +0300

    2 HW done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+
+
+const listEl = document.querySelector('#ingredients');
+
+function createElement(arr) {
+    let itemsArr = [];
+    arr.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = item;
+        itemsArr.push(listItem);
+    });
+    return listEl.prepend(...itemsArr);
+}
+
+createElement(ingredients);
