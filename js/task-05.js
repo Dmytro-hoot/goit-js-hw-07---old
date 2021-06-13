d4beca5f06d07334f76a0cd246f06c1e3c786446
Dmytro Kondratskiy <levius2@gmail.com>
2021-06-13 20:18:04 +0300

    HW 7 ex 4 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+
+function handleInputChange(event) {
+    const value = event.target.value;
+    outputRef.textContent = value || 'незнакомец';
+}
+
+inputRef.addEventListener('input', handleInputChange);
